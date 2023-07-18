b534128a8324ab7f31d3d4ad1a6bfc494194c665
shalini192224028 <123437387+shalini192224028@users.noreply.github.com>
2023-07-18 21:24:30 +0530

    Add files via upload

---
+++ REVERSE A SENTENCE.py
@@ -0,0 +1,9 @@
+sentence = "My Name is ABC"
+
+words = sentence.split()
+
+reversed_words = words[::-1]
+
+new_sentence = ' '.join(reversed_words)
+
+print("The reversed sentence is:", new_sentence)
