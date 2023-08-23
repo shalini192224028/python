dceffcd693392d3c7855ca836266abd8dbd06473
shalini192224028 <123437387+shalini192224028@users.noreply.github.com>
2023-08-23 20:15:49 +0530

    Add files via upload

---
+++ Day 5.3(1).py
@@ -0,0 +1,14 @@
+def perfectsquare(n):
+    res=n
+    for i in range(1,n+1):
+        temp=i*i
+        if temp>n:
+            break
+        else:
+            res=min(res,1+perfectsquare(n-temp))
+    return res
+n=int(input("enter a number: "))
+if n<=3:
+    print("1")
+else:
+    print(perfectsquare(n))
